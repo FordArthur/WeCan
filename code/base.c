8b35f03e71e0e997cfa871d0d22dfc58eea99b98
Ford Arthur <jajasaludos.112358@gmail.com>
2025-03-25 07:33:05 +0100

    Debugging y cambios al diseño

--- code/base.c
+++ code/base.c
@@ -1,12 +1,13 @@
 /** Solaris:
  * Descripción: Receptor/transmisor de la base
  * Autor: Marcos Ávila Navas
- * Version 1.0.2
+ * Version 2.0.0
  *         | | +-- Avances
  *         | +---- Cambios al diseño
  *         +------ Cambios al protocolo
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -14,21 +15,26 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdint.h>
+
+#define HEADER "Tiempo (s),Temperatura (ºC),Presión (Pa),Altitud (m),Nivel de CO (ppm)\n"
+
+typedef struct Chunk {
+  uint16_t time;
+  float temp;
+  float pres;
+  float monox;
+} Chunk;
 
 /** set_interface_attribs : int, int, int -> int
  * Ajusta el puerto para tener la interfaz apropiada para la lectura de datos
  */
-static
 int set_interface_attribs (int fd, int speed, int parity) {
   /* Los detalles no son importantes, pero básicamente configuramos una interfaz
-   * no-canónica, con VMIN = 0 y VTIME = 5
+   * no-canónica, con VMIN = 0 y VTIME = 20
    */
   struct termios tty;
   if (tcgetattr (fd, &tty) != 0) {
-    fprintf(stderr, "error %d from tcgetattr while setting up interface: %s\n", errno, strerror(errno));
+    fprintf(stderr, "error %d from tcgetattr\n", errno);
     return -1;
   }
 
@@ -39,8 +45,8 @@ int set_interface_attribs (int fd, int speed, int parity) {
   tty.c_iflag &= ~IGNBRK;
   tty.c_lflag = 0;
   tty.c_oflag = 0;
-  tty.c_cc[VMIN] = 0;
-  tty.c_cc[VTIME] = 5;
+  tty.c_cc[VMIN]  = 0;
+  tty.c_cc[VTIME] = 20;
   tty.c_iflag &= ~(IXON | IXOFF | IXANY);
   tty.c_cflag |= (CLOCAL | CREAD);
   tty.c_cflag &= ~(PARENB | PARODD);
@@ -49,143 +55,57 @@ int set_interface_attribs (int fd, int speed, int parity) {
   tty.c_cflag &= ~CRTSCTS;
 
   if (tcsetattr (fd, TCSANOW, &tty) != 0) {
-    fprintf(stderr, "error %d from tcsetattr while setting up interface: %s\n", errno, strerror(errno));
+    fprintf(stderr, "error %d from tcsetattr\n", errno);
     return -1;
   }
   return 0;
 }
 
-typedef struct Reader {
-  uint64_t chunk_size;
-  const char *serial, *file;
-} Reader;
-
-pthread_mutex_t serial_lock = PTHREAD_MUTEX_INITIALIZER;
-
 static
-void* reader(void* arg) {
-  const Reader* conf = arg;
-  ssize_t read_buf_s = conf->chunk_size
-        , read_s;
-  char read_buf[read_buf_s];
-
-  int serial_fd = open(conf->serial, O_RDONLY | O_NOCTTY | O_SYNC);
-  if (serial_fd < 0) {
-    fprintf(stderr, "(Reader) error %d opening serial: %s\n", errno, strerror(errno));
-    return NULL;
-  }
-
-  int file_fd = open(conf->file, O_WRONLY | O_CREAT | O_APPEND);
-  if (serial_fd < 0) {
-    fprintf(stderr, "(Reader) error %d opening file: %s\n", errno, strerror(errno));
-    return NULL;
-  }
-
-  while (1) {
-    pthread_mutex_lock(&serial_lock);
-    if ((read_s = read(serial_fd, read_buf, read_buf_s)) < 0) {
-      fprintf(stderr, "(Reader) error %d reading from serial: %s\n", errno, strerror(errno));
-    }
-    pthread_mutex_unlock(&serial_lock);
-
-    if (write(file_fd, read_buf, read_s) < 0) {
-      fprintf(stderr, "(Reader) error %d writing to file: %s\n", errno, strerror(errno));
-    }
-  }
+int report(FILE* src, FILE* dst) {
+  Chunk chunk;
+  fread(&chunk, sizeof(Chunk), 1, src);
+  const int ret = fprintf(dst, "%d,%f,%f,%f\n", chunk.time, chunk.temp, chunk.pres, chunk.monox);
+  fprintf(stdout, "%d,%f,%f,%f\n", chunk.time, chunk.temp, chunk.pres, chunk.monox);
+  return ret;
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 3 || 4 < argc) {
+  if (argc < 3 && 4 > argc) {
     fprintf(stderr, "Usage: %s [port] [file] [chunk_size?]\n", argv[0]);
     return 1;
   }
-
-  pthread_t thread;
-  Reader conf = (Reader) {
-    .chunk_size = argc == 3? 16 : atoi(argv[3]),
-    .serial= argv[1],
-    .file = argv[2]
-  };
-
-  if (pthread_create(&thread, NULL, reader, &conf) < 0) {
-    fprintf(stderr, "error %d creating reader thread: %s\n", errno, strerror(errno));
-    return 1;
-  }
-
-  int serial_fd  = open(argv[1], O_WRONLY | O_NOCTTY | O_SYNC);
-  if (serial_fd < 0) {
+  FILE* serial  = fopen(argv[1], "r");
+  if (!serial) {
     fprintf(stderr, "error %d opening %s: %s\n", errno, argv[1], strerror(errno));
     return 1;
   }
-  if (set_interface_attribs(serial_fd, B9600, 0) < 0) return 1;
-
-  int file_fd = open(argv[2], O_RDONLY | O_CREAT, 0644);
-  if (serial_fd < 0) {
+  FILE* file = fopen(argv[2], "w+");
+  if (!file) {
     fprintf(stderr, "error %d opening %s: %s\n", errno, argv[2], strerror(errno));
     return 1;
   }
 
-  struct termios stdin_settings;
-  if (tcgetattr(STDIN_FILENO, &stdin_settings) != 0) {
-    fprintf(stderr, "error %d from tcsetattr while setting up stdin: %s\n", errno, strerror(errno));
-    return 1;
-  }
-  stdin_settings.c_lflag &= ~(ICANON | ECHO);
-  if (tcsetattr(STDIN_FILENO, TCSANOW, &stdin_settings) != 0) {
-    fprintf(stderr, "error %d from tcsetattr while setting up stdin: %s\n", errno, strerror(errno));
-    return 1;
+  set_interface_attribs(fileno(serial), B9600, 0);
+
+  long read_buf_s = argc == 3? 16 : atoi(argv[3])
+     , read_s;
+  uint32_t recv = 0
+         , top;
+  char read_buf[read_buf_s];
+
+  printf(
+    "\033c"
+    "=== Programa de lectura WeCan ===\n\n"
+    HEADER
+  );
+
+  fprintf(file, HEADER);
+
+  while (1) if (fread(&top, sizeof(uint32_t), 1, serial)) {
+    fwrite(&recv, sizeof(uint32_t), 1, serial);
+
+    while (recv < top && report(serial, file)) recv++;
   }
 
-  int64_t read_s;
-  char read_buf[1024]
-     , in;
-
-  while (1) {
-    if (read(STDIN_FILENO, &in, 1) > 0) switch (in) {
-      case 'q':
-        pthread_kill(thread, SIGKILL);
-        close(serial_fd);
-        close(file_fd);
-        return 0;
-      case 'c':
-        printf("\033[H\033[J");
-        fflush(stdout);
-        break;
-      case 's':
-        printf("> ");
-        for (read_s = 0; (in = getchar()) != '\n' && read_s < 1023; ++read_s) {
-          putchar(in);
-          read_buf[read_s] = in;
-        }
-        putchar('\n');
-        pthread_mutex_lock(&serial_lock);
-        if (write(serial_fd, read_buf, read_s) < 0) {
-          fprintf(stderr, "error %d writting to serial: %s", errno, strerror(errno));
-        }
-        pthread_mutex_unlock(&serial_lock);
-        break;
-      case 'l':
-        lseek(file_fd, 0, SEEK_SET);
-        while ((read_s = read(file_fd, read_buf, 1024)) > 0) {
-          write(STDOUT_FILENO, read_buf, read_s);
-        } putchar('\n');
-        fflush(stdout);
-        if (read_s < 0) {
-          fprintf(stderr, "error %d reading capture: %s\n", errno, strerror(errno));
-        }
-        break;
-      case 'h':
-      default:
-        printf(
-          "===============================================================================================================\n"
-          "|| WeCan programa base: Controles                                                                            ||\n"
-          "||                                                                                                           ||\n"
-          "|| q: cierra este programa    s: manda un mensaje por la antena    l: enseña los contenidos de la captura    ||\n"
-          "|| c: limpia la pantalla      h: muestra este mensaje                                                        ||\n"
-          "===============================================================================================================\n"
-          "\n"
-        );
-        break;
-    }
-  }
 }
